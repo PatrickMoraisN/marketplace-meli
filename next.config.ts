c14ce5997c17a0594185ae6dd65fe7536cb332d2
Patrick Morais <ppternunes@gmail.com>
2025-10-22 20:33:41 -0300

    chore: next config for images domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    domains: ['http2.mlstatic.com'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
