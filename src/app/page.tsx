c7de7ae7284668431038fd40b1dffde29e822fad
Patrick Morais <ppternunes@gmail.com>
2025-10-24 17:59:22 -0300

    delete default css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 'use client'
 
 import { SearchHeader } from '@/shared/ui/'
-import './page.scss'
 
 export default function Home() {
   return (
     <>
       <SearchHeader />
-      <div className="page__text">bem vindo</div>
     </>
   )
 }
