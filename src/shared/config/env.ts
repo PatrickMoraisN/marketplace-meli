81a6238ec4fcfaa19a14658561984efe5f130c9a
Patrick Morais <ppternunes@gmail.com>
2025-10-24 15:39:12 -0300

    chore: add public site url env

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
+  NEXT_PUBLIC_SITE_URL: z.string().url().default('http://localhost:3000'),
 })
 
 const parsed = envSchema.safeParse(process.env)
