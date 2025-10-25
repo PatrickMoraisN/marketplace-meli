ff442a6adc418d10878caa1e71186aba72502964
Patrick Morais <ppternunes@gmail.com>
2025-10-24 22:47:01 -0300

    fix styles

--- src/app/items/[id]/page-skeleton.tsx
+++ src/app/items/[id]/page-skeleton.tsx
@@ -12,43 +12,58 @@ export function ProductPageSkeleton() {
 
       <div className={styles.container}>
         <div className={styles.breadcrumb}>
-          <Skeleton width={300} height={16} />
+          <Skeleton width={280} height={14} />
         </div>
 
         <Paper className={styles.paper}>
           <div className={styles.productSection}>
             <div className={styles.gallerySkeleton}>
               <Skeleton width="100%" height={400} radius={8} />
-              <div className={styles.thumbnailRow}>
-                {Array.from({ length: 4 }).map((_, i) => (
-                  <Skeleton key={i} width={64} height={64} radius={8} />
-                ))}
-              </div>
             </div>
 
             <div className={styles.detailsSkeleton}>
-              <Skeleton width={160} height={14} />
-              <Skeleton width="90%" height={32} />
-              <Skeleton width="70%" height={28} />
-              <Skeleton width={200} height={16} />
-              <Skeleton width={220} height={44} />
-              <Skeleton width="80%" height={16} />
+              <div className={styles.titleBlock}>
+                <Skeleton width="80%" height={28} />
+                <Skeleton width="60%" height={18} />
+              </div>
+
+              <div className={styles.priceBlock}>
+                <Skeleton width={160} height={36} />
+                <Skeleton width={220} height={44} />
+              </div>
+
+              <div className={styles.shippingBlock}>
+                <Skeleton width={120} height={16} />
+                <Skeleton width={160} height={16} />
+              </div>
+
+              <div className={styles.actionsBlock}>
+                <Skeleton width="60%" height={44} radius={8} />
+                <Skeleton width="40%" height={44} radius={8} />
+              </div>
+
+              <div className={styles.sellerBlock}>
+                <Skeleton width="70%" height={14} />
+                <Skeleton width="50%" height={14} />
+              </div>
             </div>
           </div>
 
+          <div className={styles.divider} />
+
           <div className={styles.descriptionSkeleton}>
-            <Skeleton width={140} height={22} />
-            {Array.from({ length: 4 }).map((_, i) => (
-              <Skeleton key={i} width={`${95 - i * 3}%`} height={16} />
+            <Skeleton width={180} height={22} />
+            {Array.from({ length: 5 }).map((_, i) => (
+              <Skeleton key={i} width={`${100 - i * 5}%`} height={16} />
             ))}
           </div>
 
           <div className={styles.characteristicsSkeleton}>
-            <Skeleton width={180} height={22} />
+            <Skeleton width={200} height={22} />
             <div className={styles.characteristicsGrid}>
-              {Array.from({ length: 6 }).map((_, i) => (
+              {Array.from({ length: 8 }).map((_, i) => (
                 <div key={i} className={styles.characteristicsItem}>
-                  <Skeleton width="60%" height={14} />
+                  <Skeleton width="50%" height={14} />
                   <Skeleton width="80%" height={14} />
                 </div>
               ))}
